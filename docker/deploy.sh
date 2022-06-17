829a0a77cc1da0689b9846c0f5ec3c5b1325536f
Harald Lurger <harald.lurger@logicals.com>
2022-06-17 13:30:07 +0200

    CLOUD-1659: Deploy Artifacts
    Change-Id: I4a69acb39c87c6fcacbf3974ed4e9d449b8af2a6

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-mvn -B clean deploy -PcreateSourceZip,downloadNecessaryArtifacts | tee /tmp/build.log
+# mvn -B clean deploy -PcreateSourceZip,downloadNecessaryArtifacts | tee /tmp/build.log
+mvn -B clean deploy | tee /tmp/deploy.log
